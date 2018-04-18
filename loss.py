cff1bed5c75bf0ff5fc1ff2f3ad27c351ae12a49
Roland Laboulaye <rlaboulaye@gmail.com>
2018-04-18 02:32:16 -0600

    adjusted kld loss annealing rate

--- loss.py
+++ loss.py
@@ -9,11 +9,10 @@ class SequenceVariationalLoss(nn.Module):
 
 	def __init__(self):
 		super(SequenceVariationalLoss, self).__init__()
-		self.reconstruction_coefficient = 79
 		self.reconstruction_loss = CrossEntropyLoss()
 
-	def _get_kld_coefficient(self, iteration):
-		return (math.tanh((iteration - 3500) / 1000) + 1) / 2
+	def _get_kld_coefficient(self, i):
+		return (math.tanh((i - 142500) / 30000) + 1) / 2
 
 	def kld_loss(self, mu, logvar):
 		return (-0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), 1)).mean()
@@ -25,6 +24,5 @@ class SequenceVariationalLoss(nn.Module):
 			reconstruction_losses.append(batch_reconstruction_loss)
 		kld_loss = self.kld_loss(mu, logvar)
 		reconstruction_loss = torch.cat(reconstruction_losses).mean()
-		loss = (self.reconstruction_coefficient * reconstruction_loss) + \
-				(self._get_kld_coefficient(iteration) * kld_loss)
-		return loss
+		loss = reconstruction_loss + (self._get_kld_coefficient(iteration) * kld_loss)
+		return loss, reconstruction_loss, kld_loss
