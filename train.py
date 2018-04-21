0f0d6df325a2e15ffb065cd2560559f0675f7aa7
Roland Laboulaye <rlaboulaye@gmail.com>
2018-04-21 04:39:00 -0600

    ready for demo

--- train.py
+++ train.py
@@ -6,15 +6,29 @@ use_context_enhanced_rnn = True
 use_pretrained_weights = True
 hvae = HierarchicalVariationalAutoEncoder(vocab_size=vocab_size, drop_prob=drop_prob, \
 		use_context_enhanced_rnn=use_context_enhanced_rnn, use_pretrained_weights=use_pretrained_weights, \
-		max_rows=None, max_sentences_in_paragraph_loading=2e5, max_paragraphs=2e5)
+		max_rows=5e3, max_sentences_in_paragraph_loading=5e3, max_paragraphs=5e3)
 
 # hvae.train_vae()
 
 # hvae.train_guide()
 
-# print('\nGENERATE')
-# for sentence in hvae.generate_sentence():
+# print('\nGENERATE SENTENCE')
+# for sentence in hvae.generate_sentence(35):
 #        print(sentence)
+
+# print('\nGENERATE PARAGRAPH')
+# hvae.generate_paragraph("yes , you did .")
+# print()
+# hvae.generate_paragraph("he asked , knowing she was n't .")
+# print()
+# hvae.generate_paragraph("what are you two doing up here ? .")
+# print()
+# hvae.generate_paragraph("you need someone down by the door .")
+# print()
+# hvae.generate_paragraph("`` what did you do now ? '' .")
+# print()
+# hvae.generate_paragraph("i miss him so much .")
+# print()
 
 # print('\nINTERPOLATE')
 # for sentence in hvae.interpolate("`` i want to talk to you '' .", "she did n't want to be with him ."):
@@ -22,6 +36,28 @@ hvae = HierarchicalVariationalAutoEncoder(vocab_size=vocab_size, drop_prob=drop_
 
 # for sentence in hvae.interpolate("he was silent for a long moment .", "it was my turn ."):
 # 	print(sentence)
+
+# for sentence in hvae.interpolate("he looked at me .", "then he kissed me ."):
+# 	print(sentence)
+
+# for sentence in hvae.interpolate("he is my best friend .", "we had a big fight ."):
+# 	print(sentence)
+
+# for sentence in hvae.interpolate("yes , you did .", "are you serious ? ."):
+# 	print(sentence)
+# print()
+# for sentence in hvae.interpolate("he asked , knowing she was n't .", "then he walked away ."):
+# 	print(sentence)
+# print()
+# for sentence in hvae.interpolate("what are you two doing up here ? .", "its just that ! ."):
+# 	print(sentence)
+# print()
+# for sentence in hvae.interpolate("you need someone down by the door .", "how did you find her ? ."):
+# 	print(sentence)
+# print()
+# for sentence in hvae.interpolate("`` what did you do now ? '' .", "you could have done as well '' ."):
+# 	print(sentence)
+# print()
 
 # print('\nRECONSTRUCT')
 # print(hvae.reconstruct("the man is very angry ."))
